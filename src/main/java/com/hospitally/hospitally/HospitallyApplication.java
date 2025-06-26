db025d0cfd3f5726575f0484978c7d57e886722e
Anthony Alikah <a.o.alikah@gmail.com>
2025-06-26 14:23:30 +0100

    Restart with java 24

---
+++ src/main/java/com/hospitally/hospitally/HospitallyApplication.java
@@ -0,0 +1,13 @@
+package com.hospitally.hospitally;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitallyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitallyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hospitally/hospitally/HospitallyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospitally.hospitally;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitallyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
