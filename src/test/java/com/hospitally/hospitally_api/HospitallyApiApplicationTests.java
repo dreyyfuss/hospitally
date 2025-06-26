81cbbabbfabe2798bc08dc215532432cbca7b4c5
Anthony Alikah <a.o.alikah@gmail.com>
2025-06-26 11:46:46 +0100

    Initialize spring

---
+++ src/test/java/com/hospitally/hospitally_api/HospitallyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospitally.hospitally_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitallyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
