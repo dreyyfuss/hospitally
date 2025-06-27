dfa319a32cb3c0f2e5f0428b5e5062ff18118565
Abdulazeez ibrahim <109610178+tech2world@users.noreply.github.com>
2025-06-27 14:53:03 -0700

    Update PatientController.java

--- src/main/java/com/hospitally/hospitally/controller/PatientController.java
+++ src/main/java/com/hospitally/hospitally/controller/PatientController.java
@@ -1,4 +1,34 @@
 package com.hospitally.hospitally.controller;
 
+import com.hospitally.hospitally.dto.request.patient.CreatePatientRequest;
+import com.hospitally.hospitally.dto.response.ApiResponse;
+import com.hospitally.hospitally.dto.response.patient.PatientResponse;
+import com.hospitally.hospitally.service.PatientService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
 public class PatientController {
+
+    private final PatientService patientService;
+    @Autowired
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @PostMapping("/create-patient")
+    public ResponseEntity<ApiResponse<PatientResponse>> createPatient(@RequestBody @Valid CreatePatientRequest request) {
+        ApiResponse<PatientResponse> response = patientService.createPatient(request);
+        return ResponseEntity.status(response.getStatusCode().equals
+                        ("00") ? HttpStatus.CREATED : HttpStatus.BAD_REQUEST)
+                .body(response);
+    }
 }
