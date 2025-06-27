e3dbfc523692db161a2f8ad2b64a67f68c241b0e
Abdulazeez ibrahim <109610178+tech2world@users.noreply.github.com>
2025-06-27 14:53:42 -0700

    Update PatientResponse.java

--- src/main/java/com/hospitally/hospitally/dto/response/patient/PatientResponse.java
+++ src/main/java/com/hospitally/hospitally/dto/response/patient/PatientResponse.java
@@ -1,4 +1,10 @@
 package com.hospitally.hospitally.dto.response.patient;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class PatientResponse {
+    private String message;
 }
