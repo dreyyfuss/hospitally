8e27ce36428bcce26eb47d8086f4835b9869ded6
Abdulazeez ibrahim <109610178+tech2world@users.noreply.github.com>
2025-06-27 17:11:41 -0700

    Update PatientRepository.java

--- src/main/java/com/hospitally/hospitally/repository/database/interfaces/PatientRepository.java
+++ src/main/java/com/hospitally/hospitally/repository/database/interfaces/PatientRepository.java
@@ -2,6 +2,11 @@ package com.hospitally.hospitally.repository.database.interfaces;
 
 import com.hospitally.hospitally.model.entity.Patient;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PatientRepository {
     int createPatient(Patient patient);
+    List<Patient> findAllPatients();
+    Optional<Patient> findPatientById(int id);
 }
