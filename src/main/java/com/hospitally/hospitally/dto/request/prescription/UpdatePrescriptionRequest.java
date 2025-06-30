7a08a2ecb94614ced7eb2e3fce78a78ce16c0869
Abdulazeez ibrahim <109610178+tech2world@users.noreply.github.com>
2025-06-30 16:50:29 -0700

    Add Lombok annotations and fields to UpdatePrescriptionRequest
    Introduced Lombok annotations (@Data, @Builder, @NoArgsConstructor, @AllArgsConstructor) to reduce boilerplate code. Added 'comment' and 'status' fields to the UpdatePrescriptionRequest class.

--- src/main/java/com/hospitally/hospitally/dto/request/prescription/UpdatePrescriptionRequest.java
+++ src/main/java/com/hospitally/hospitally/dto/request/prescription/UpdatePrescriptionRequest.java
@@ -1,4 +1,15 @@
 package com.hospitally.hospitally.dto.request.prescription;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UpdatePrescriptionRequest {
+    private String comment;
+    private String status;
 }
