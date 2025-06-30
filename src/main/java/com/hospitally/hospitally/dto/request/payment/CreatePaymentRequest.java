fa759b15eee17b5ab3628e1cc5966b6f8375061f
Abdulazeez ibrahim <109610178+tech2world@users.noreply.github.com>
2025-06-30 10:23:40 -0700

    Add fields and Lombok annotations to CreatePaymentRequest
    Introduced Lombok annotations for boilerplate code reduction and added paymentTime, paymentReference, and paymentAmount fields to the CreatePaymentRequest DTO.

--- src/main/java/com/hospitally/hospitally/dto/request/payment/CreatePaymentRequest.java
+++ src/main/java/com/hospitally/hospitally/dto/request/payment/CreatePaymentRequest.java
@@ -1,4 +1,19 @@
 package com.hospitally.hospitally.dto.request.payment;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreatePaymentRequest {
+    private LocalDateTime paymentTime;
+    private String paymentReference;
+    private BigDecimal paymentAmount;
 }
