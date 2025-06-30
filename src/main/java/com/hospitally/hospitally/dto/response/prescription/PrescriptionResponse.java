c6a992fbcfb6a890648ac608c9107a8a72789f04
Abdulazeez ibrahim <109610178+tech2world@users.noreply.github.com>
2025-06-30 16:49:50 -0700

    Add Lombok annotations and fields to PrescriptionResponse
    Introduced Lombok annotations (@Data, @Builder, @NoArgsConstructor, @AllArgsConstructor) to reduce boilerplate code. Added fields for prescriptionId, appointmentId, comment, status, createdAt, updatedAt, and message to PrescriptionResponse.

--- src/main/java/com/hospitally/hospitally/dto/response/prescription/PrescriptionResponse.java
+++ src/main/java/com/hospitally/hospitally/dto/response/prescription/PrescriptionResponse.java
@@ -1,4 +1,22 @@
 package com.hospitally.hospitally.dto.response.prescription;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PrescriptionResponse {
+    private Integer prescriptionId;
+    private Integer appointmentId;
+    private String comment;
+    private String status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private String message;
 }
