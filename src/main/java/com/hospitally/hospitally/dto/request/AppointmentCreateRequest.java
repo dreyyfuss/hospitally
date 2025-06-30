13ab1d1a73ffae9cce4b2b77f2ed1b5e81fbcf67
Temi <temiloluwaodunuga13@gmail.com>
2025-06-30 12:29:43 +0100

    added a noArgsconstructor for the class and created the update request class

---
+++ src/main/java/com/hospitally/hospitally/dto/request/AppointmentCreateRequest.java
@@ -0,0 +1,18 @@
+package com.hospitally.hospitally.dto.request;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.sql.Time;
+import java.util.Date;
+
+@Data
+@Builder
+public class AppointmentCreateRequest
+{
+    private int appointmentPatientId;
+
+    private Date appointmentDate;
+
+    private Time appointmentTime;
+}
