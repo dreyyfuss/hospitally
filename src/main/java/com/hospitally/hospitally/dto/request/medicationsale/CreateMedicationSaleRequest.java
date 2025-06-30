810622401c13cdc5aed004bff34aeae8e797945f
Abdulazeez ibrahim <109610178+tech2world@users.noreply.github.com>
2025-06-30 16:51:17 -0700

    Add Lombok annotations and fields to CreateMedicationSaleRequest
    Introduced Lombok annotations (@Data, @Builder, @AllArgsConstructor, @NoArgsConstructor) to reduce boilerplate code and added saleId, medicationId, and quantity fields to the CreateMedicationSaleRequest class.

--- src/main/java/com/hospitally/hospitally/dto/request/medicationsale/CreateMedicationSaleRequest.java
+++ src/main/java/com/hospitally/hospitally/dto/request/medicationsale/CreateMedicationSaleRequest.java
@@ -1,4 +1,16 @@
 package com.hospitally.hospitally.dto.request.medicationsale;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateMedicationSaleRequest {
+    private Integer saleId;
+    private Integer medicationId;
+    private Integer quantity;
 }
