5c90a4a2ddca185a9cc9bbe603d6dd405821d46d
Abdulazeez ibrahim <109610178+tech2world@users.noreply.github.com>
2025-06-29 18:24:56 -0700

    Update UpdateDoctorRequest.java

--- src/main/java/com/hospitally/hospitally/dto/request/doctor/UpdateDoctorRequest.java
+++ src/main/java/com/hospitally/hospitally/dto/request/doctor/UpdateDoctorRequest.java
@@ -1,4 +1,15 @@
 package com.hospitally.hospitally.dto.request.doctor;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UpdateDoctorRequest {
+    private String specialization;
+    private String status;
 }
