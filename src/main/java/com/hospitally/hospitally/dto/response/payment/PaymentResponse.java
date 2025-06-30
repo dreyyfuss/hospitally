8cdadcb3a71fedb27488b094fed74c0db7001907
Abdulazeez ibrahim <109610178+tech2world@users.noreply.github.com>
2025-06-30 10:23:20 -0700

    Add fields and Lombok annotations to PaymentResponse
    Introduced Lombok annotations (@Data, @Builder, @AllArgsConstructor, @NoArgsConstructor) and added fields for payment details, timestamps, and message to the PaymentResponse DTO.

--- src/main/java/com/hospitally/hospitally/dto/response/payment/PaymentResponse.java
+++ src/main/java/com/hospitally/hospitally/dto/response/payment/PaymentResponse.java
@@ -1,4 +1,24 @@
 package com.hospitally.hospitally.dto.response.payment;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PaymentResponse {
+    private Integer paymentId;
+    private LocalDateTime paymentTime;
+    private String paymentReference;
+    private BigDecimal paymentAmount;
+    private String paymentStatus;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private String message;
 }
